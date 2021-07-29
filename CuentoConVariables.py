0f73cb79940ef372790419b095a5a51c9b9172b1
Humberto Chávez <humberto.chavez@gmail.com>
2021-07-29 16:58:34 -0600

    Create CuentoConVariables.py

---
+++ CuentoConVariables.py
@@ -0,0 +1,11 @@
+# Programa Cuento utilizando variables
+
+nombre_del_personaje = "Pedro"
+edad_del_personaje = "85"
+ciudad = "Ojinaga"
+genero_musical = "Banda"
+print ("Había una vez un alumno del CBTis 122 que se llamaba " + nombre_del_personaje)
+print ("tenía " + edad_del_personaje, "años y vivía al norte de la ciudad de " + ciudad)
+print ("A " + nombre_del_personaje + " le gustaba oir música de", genero_musical, "era su género musical favorito.")
+print ( nombre_del_personaje + " trabajaba de cajero en un Oxxo por las tardes lo que le permitía ganar dinero")
+print ( nombre_del_personaje + " era feliz")
